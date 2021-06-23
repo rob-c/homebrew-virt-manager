7620cf1a4111cc7f700d62e6302994a81cd394cb
Robert Currie <rcurrie@cern.ch>
2021-06-23 14:17:49 +0100

    Update virt-viewer.rb

--- virt-viewer.rb
+++ virt-viewer.rb
@@ -4,7 +4,11 @@ class VirtViewer < Formula
   url "https://virt-manager.org/download/sources/virt-viewer/virt-viewer-10.0.tar.xz"
   sha256 "d23bc0a06e4027c37b8386cfd0286ef37bd738977153740ab1b6b331192389c5"
 
+  depends_on "intltool" => :build
+  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
 
   depends_on "atk"
   depends_on "cairo"
@@ -13,21 +17,23 @@ class VirtViewer < Formula
   depends_on "glib"
   depends_on "gtk+3"
   depends_on "gtk-vnc"
+  depends_on "hicolor-icon-theme"
+  depends_on "libvirt"
   depends_on "libvirt-glib"
   depends_on "pango"
+  depends_on "shared-mime-info"
   depends_on "spice-gtk"
   depends_on "spice-protocol"
 
+
   def install
-    args = %W[
-      --disable-silent-rules
-      --disable-update-mimedb
-      --with-gtk-vnc
-      --with-spice-gtk
-      --prefix=#{prefix}
-    ]
-    system "./configure", *args
-    system "make", "install"
+    system "git init"
+    system "git add ."
+    system "git commit -m 'test'"
+    system "sed -i '' -e '/git_werror/s/auto/disabled/g' meson_options.txt"
+    system "PATH=$PATH:/usr/local/bin meson --prefix=#{prefix} build/native --buildtype release"
+    system "PATH=$PATH:/usr/local/bin ninja -vC build/native"
+    system "PATH=$PATH:/usr/local/bin ninja -vC build/native install"
   end
 
   def post_install
